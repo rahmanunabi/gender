0abd2ce46954c281e6d7dec3a0f105cbf174cf7e
rahmanunabi <102538438+rahmanunabi@users.noreply.github.com>
2022-05-24 16:41:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var gender = prompt ("enter your gender");
+var male = "Sir";
+var female = "Ma'am";
+if(gender === "male"){
+    document.write("<h1>" + "Good Morning " + male + "</h1>");
+} 
+if(gender == "female"){
+    document.write("<h1>" + "Good Morning " + female + "</h1>");
+};
+
+
+
